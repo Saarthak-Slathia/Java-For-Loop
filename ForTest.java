c7083495cca9e39613510844d440033f87915a07
Saarthak Slathia <77338782+Saarthak-Slathia@users.noreply.github.com>
2021-06-16 12:14:40 +0530

    Add files via upload

---
+++ ForTest.java
@@ -0,0 +1,10 @@
+public class ForTest {
+    public static void main(String[] args) {
+        int x;
+
+        // for (x = 0; x < 10; x++) <-- Also written using increment operator.
+        for (x = 0; x < 10; x = x + 1){
+            System.out.println("This is x: " + x);
+        }
+    }
+}
